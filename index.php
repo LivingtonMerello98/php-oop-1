e2caac21f512d4a5fd8abedd730a13f49a79298f
LivingtonMerello98 <livington.merello@gmail.com>
2024-06-24 20:55:37 +0200

    [upd] class movie constructor

--- index.php
+++ index.php
@@ -6,4 +6,12 @@ class Movie
     public $title;
     public $description;
     public $releaseDate;
+
+    //class movie constructor
+    public function __construct($title, $description, $releaseDate)
+    {
+        $this->title = $title;
+        $this->description = $description;
+        $this->releaseDate = $releaseDate;
+    }
 }
