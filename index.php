ffb51bde61862598b6e879ad0449fa61411f8296
LivingtonMerello98 <livington.merello@gmail.com>
2024-06-24 22:24:38 +0200

    [upd] Instantiated three Movie objects with descriptive variable names and printed their details

--- index.php
+++ index.php
@@ -18,8 +18,19 @@ class Movie
     //funzione per stampare su schermo
     public function print()
     {
-        echo "Title: " . $this->title;
-        echo "Description: " . $this->description;
-        echo "Release Date: " . $this->releaseDate;
+        echo "Title: " . $this->title . ' ';
+        echo "Description: " . $this->description . ' ';
+        echo "Release Date: " . $this->releaseDate . ' ';
+        echo '<br>';
     }
 }
+
+
+$movie1 = new Movie('Transformers', 'lorem ipsum', '2012');
+$movie2 = new Movie('I am Legend', 'lorem ipsum', '2012');
+$movie3 = new Movie('E.T', 'lorem ipsum', '2012');
+
+
+$movie1->print();
+$movie2->print();
+$movie3->print();
